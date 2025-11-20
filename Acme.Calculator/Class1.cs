ab2e2ea5c42c1156b4f6258b6acd809468c76b4b
Mirjahon Istamov <istamovmirjahon8@gmail.com>
2025-11-20 16:43:22 +0500

    Add basic math methods and adjust build warning settings
    Added `Add` and `Subtract` methods to `Class1` in the
    `Acme.Calculator` namespace, along with integer fields `a`
    and `b` initialized to 3 and 4, respectively.
    
    Updated `Directory.Build.props` to set `<TreatWarningsAsErrors>`
    to `false`, allowing warnings to no longer block builds.

--- Acme.Calculator/Class1.cs
+++ Acme.Calculator/Class1.cs
@@ -2,4 +2,13 @@ namespace Acme.Calculator;
 
 public class Class1
 {
+    int a =3, b=4;
+    public int Add()
+    {
+        return a + b;
+    }
+    public int Subtract()
+    {
+        return a - b;
+    }
 }
